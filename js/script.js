acb89137392fd4f010c87304948249a0bc207b99
yeon-bo <dylk97@gmail.com>
2021-11-30 20:48:21 +0900

    feat: side screen setting _ js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
+function setMenu(_menu) {
+    var filterButtons = document.querySelectorAll("nav li");
+    filterButtons.forEach(function (filterButton) {
+      filterButton.classList.remove("on");
+    });
+
+    document.querySelector("nav li." + _menu).classList.add("on");
+
+    document.querySelector("main").className = _menu;
+}
 
 
