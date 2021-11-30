b39f434a33231f78a93801bb6c497c8263abe5bc
yeon-bo <dylk97@gmail.com>
2021-11-30 21:11:26 +0900

    feat: upload page _ js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,14 @@ function setMenu(_menu) {
     });
 
     document.querySelector("nav li." + _menu).classList.add("on");
-
     document.querySelector("main").className = _menu;
 }
 
 
+function setDescLength () {
+    document.querySelector(".descLength").innerHTML =
+     document.querySelector("input.description").value.length + "/20";
+  }
+
+  
+  
